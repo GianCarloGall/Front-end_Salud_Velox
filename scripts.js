1c524880f7689989638dd6123e85369f78bb73c9
GianCarloGall <ggallegosg1900@alumno.ipn.mx>
2021-04-30 15:32:58 -0500

    Version de Entrega

--- scripts.js
+++ scripts.js
@@ -13,3 +13,48 @@ jQuery('document').ready(function($){
         }
     });
 });
+function abrir(){
+    document.getElementById("vent").style.display="block";
+}
+function cerrar(){
+    document.getElementById("vent").style.display="none";
+}
+function inicio(){
+    location.href = "index.html"
+}
+function iniciarsesion(){
+    location.href = "IniciarSesion.html"
+}
+function crearcuenta(){
+    location.href = "CrearCuenta.html"
+}
+function codigo(){
+    location.href = "Codigo.html"
+}
+function cuenta(){
+    location.href = "Cuenta.html"
+}
+function formulario(){
+    location.href = "Formulario.html"
+}
+function olvidar(){
+    location.href = "Olvidar.html"
+}
+function resultado(){
+    location.href = "Resultado.html"
+}
+function chat(){
+    location.href = "Chat.html"
+}
+function velox(){
+    window.open("https://giancarlogall.github.io/VeloxPagina/Velox.html");
+}
+function facebook(){
+    window.open("https://www.facebook.com/VeloxSoftware");
+}
+function instagram(){
+    window.open("https://www.instagram.com/veloxsoftware/");
+}
+function twitter(){
+    window.open("https://twitter.com/SoftwareVelox");
+}
